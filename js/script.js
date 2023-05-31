9be8b553de7f6450b8c72784451e3126365b34f9
yakupozmm <yakupozmm@gmail.com>
2023-05-31 22:07:01 +0300

    proje tamam

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ function up_now(){
   console.log(count);
 
 }
+
+
+function down_now (){
+    event.preventDefault();
+  var count;
+
+  count=document.getElementById('count').value;
+  count = parseInt(count)  -1;
+  document.getElementById('count').value=count;
+
+  console.log(count);
+
+}
