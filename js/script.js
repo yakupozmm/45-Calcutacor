849b4aad150c094acabdf0d2a0a4aaf84c39db98
yakupozmm <yakupozmm@gmail.com>
2023-05-31 22:03:32 +0300

    Artılma işi tamam

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello From Code")
+function up_now(){
+    event.preventDefault();
+  var count;
+
+  count=document.getElementById('count').value;
+  count = parseInt(count)  +1;
+  document.getElementById('count').value=count;
+
+  console.log(count);
+
+}
