30db52524be620e14136851cc89e82462f7ffe76
yakupozmm <yakupozmm@gmail.com>
2023-06-04 22:06:52 +0300

    proje tamamlandi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 function up_now(){
     event.preventDefault();
   var count;
+  document.getElementById('down').disabled=false;
 
   count=document.getElementById('count').value;
-  count = parseInt(count)  +1;
+
+
+  if(count<10){
+    count = parseInt(count)  +1;
+
+
+ }
+else{
+  alert('Sayaç 10 sayisindan büyük olamaz')
+}
+
+if(count==10){
+  document.getElementById('up').disabled=true;
+}
+  
+
   document.getElementById('count').value=count;
 
   console.log(count);
@@ -14,9 +30,17 @@ function up_now(){
 function down_now (){
     event.preventDefault();
   var count;
-
+  document.getElementById('up').disabled=false;
   count=document.getElementById('count').value;
+if(count>0){
   count = parseInt(count)  -1;
+
+}
+  else{alert('Sayaç negatif olamaz')}
+  if(count==0){
+    document.getElementById('down').disabled=true;
+  }
+
   document.getElementById('count').value=count;
 
   console.log(count);
